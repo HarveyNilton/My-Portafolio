81544f1f3c47afd83c506e1cc1f043a3ac3ae2bd
HarveyNilton <65696942+HarveyNilton@users.noreply.github.com>
2023-02-11 09:22:05 -0500

    addvideo1

--- src/components/portafolio/Portafolio.jsx
+++ src/components/portafolio/Portafolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './portafolio.css'
+import ecommerce from '/src/assets/video/ecommerce.mp4'
 
 const Portafolio = () => {
     return (
@@ -7,51 +8,91 @@ const Portafolio = () => {
             <div className="portafolio">
                 <h3 className='titulo'>Portafolio</h3>
                 <div className="container-item-portafolio">
-                    <div className="item-portafolio">
+
+                    <a className="item-portafolio" href="">
                         <div className="conatainer-img-portafolio">
-
+                            <div className='video'>
+                                <video className='digital-arch' controls>
+                                    <source  src={ecommerce} type="video/mp4" />
+                                </video>
+                        
+                            </div>
+                            <div className="description">
+                                <h4>name</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quibusdam facilis, maxime id aspernatur rem placeat?</p>
+                            </div>
                         </div>
-                        <div className="description-portafolio">
+                    </a>
 
+
+                    <div className="item-portafolio">
+
+                        <div className="conatainer-img-portafolio">
+                            <div className='video'>
+                              
+                            </div>
+                            <div className="description">
+                                <h4>name</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quibusdam facilis, maxime id aspernatur rem placeat?</p>
+                            </div>
                         </div>
                     </div>
 
                     <div className="item-portafolio">
+
                         <div className="conatainer-img-portafolio">
-
-                        </div>
-                        <div className="description-portafolio">
-
+                            <div className='video'>
+                               
+                            </div>
+                            <div className="description">
+                                <h4>name</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quibusdam facilis, maxime id aspernatur rem placeat?</p>
+                            </div>
                         </div>
                     </div>
 
                     <div className="item-portafolio">
+
                         <div className="conatainer-img-portafolio">
-
-                        </div>
-                        <div className="description-portafolio">
-
+                            <div className='video'>
+                               
+                            </div>
+                            <div className="description">
+                                <h4>name</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quibusdam facilis, maxime id aspernatur rem placeat?</p>
+                            </div>
                         </div>
                     </div>
 
                     <div className="item-portafolio">
+
                         <div className="conatainer-img-portafolio">
-
-                        </div>
-                        <div className="description-portafolio">
-
+                            <div className='video'>
+                                
+                            </div>
+                            <div className="description">
+                                <h4>name</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quibusdam facilis, maxime id aspernatur rem placeat?</p>
+                            </div>
                         </div>
                     </div>
 
                     <div className="item-portafolio">
+
                         <div className="conatainer-img-portafolio">
-
-                        </div>
-                        <div className="description-portafolio">
-
+                            <div className='video'>
+                               
+                            </div>
+                            <div className="description">
+                                <h4>name</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quibusdam facilis, maxime id aspernatur rem placeat?</p>
+                            </div>
                         </div>
                     </div>
-          
+
+
+
+
                 </div>
             </div>
 
